4433edce160280d5f935990ef6ad3fafa907086b
Irfan Ainuddin <irfandin@gmail.com>
2018-11-21 03:15:23 -0800

    Public Build
    Functional, i dont understand wy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,14 +19,15 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { ValidateService} from './services/validate.service';
 import { AuthService} from './services/auth.service';
 import {FlashMessagesModule} from 'angular2-flash-messages';
+import {AuthGuard} from './guards/auth.guard';
 
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'profile', component: ProfileComponent}
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
@@ -48,7 +49,7 @@ const appRoutes: Routes = [
     HttpClientModule
   ],
   // Service providers brought in here
-  providers: [ValidateService, AuthService],
+  providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import { map } from 'rxjs/operators';
+// import { tokenNotExpired} from 'angular2-jwt';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 
+const helper = new JwtHelperService();
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +13,10 @@ export class AuthService {
 authToken: any;
 user: any;
 
-  constructor(private http: Http) { }
+  constructor(
+    private http: Http,
+    // private jwtHelper: JwtHelperService
+    ) { }
 
   // make POSt request to register to backend API
   registerUser(user): any {
@@ -19,5 +25,44 @@ user: any;
     return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
     .pipe(map(res => res.json()));
 
+  }
+
+  authenticateUser(user): any {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
+    .pipe(map(res => res.json()));
+  }
+
+  getProfile() {
+    const headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3000/users/profile', {headers: headers})
+    .pipe(map(res => res.json()));
+
+  }
+  storeUserData(token, user) {
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  loggedIn() {
+    const isExpired = !helper.isTokenExpired(localStorage.getItem('id_token'));
+    return isExpired;
+  }
+
+  logout() {
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
   }
 }
